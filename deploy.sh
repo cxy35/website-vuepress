7efa04deb239e9667afe0ac5b556cb28535e8edd
cyx35 <799737179@qq.com>
2022-05-27 14:51:54 +0800

    u

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
-# https://vuepress.vuejs.org/zh/guide/deploy.html#github-pages
+# 部署方法1：https://vuepress.vuejs.org/zh/guide/deploy.html#github-pages
+# 手动执行该脚本完成部署，或者你可以在你的持续集成的设置中，设置在每次 push 代码时自动运行该脚本
 #!/usr/bin/env sh
 
 # 确保脚本抛出遇到的错误
@@ -24,3 +25,4 @@ git commit -m 'deploy'
 git push -f git@github.com:cxy35/website-vuepress.git master:gh-pages
 
 cd -
+rm -rf docs/.vuepress/dist
